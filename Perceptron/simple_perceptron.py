a07e47494ffc5efa840400538ab637926c386825
xpheal <xpheal@gmail.com>
2017-01-31 02:25:08 -0600

    simple done

--- Perceptron/simple_perceptron.py
+++ Perceptron/simple_perceptron.py
@@ -1,4 +1,5 @@
 import sys
+import random as rand
 
 def file_parser(file):
 	for line in file:
@@ -44,6 +45,54 @@ def get_args():
 
 	return sys.argv[1], sys.argv[2]
 
+# Assumes that label is a boolean
+class Perceptron:
+	def __init__(self, num_features, feature_names, feature_values, label_values, learning_rate=0.1):
+		self.num_features = num_features
+		self.feature_names = {name: idx for idx, name in enumerate(feature_names)}
+		self.feature_values = [{name: idx for idx, name in enumerate(values)} for values in feature_values]
+		self.label_values = {name: idx for idx, name in enumerate(label_values)}
+		
+		self.weights = [rand.random() for i in range(num_features + 1)]
+		self.bias, self.threshold = rand.random(), 0
+		self.learning_rate = learning_rate
+
+	def labels_to_numbers(self, labels):
+		return [self.label_values[label] for label in labels]
+
+	def features_to_numbers(self, features):
+		return [[self.feature_values[idx][feature] for idx, feature in enumerate(row)]for row in features]
+
+	# train_set is a list of lists with labels and features [[label, feature1, ...], ...]
+	def train(self, train_set):
+		labels = self.labels_to_numbers([row[0] for row in train_set])
+		features = self.features_to_numbers([row[1:] for row in train_set])
+
+		for idx, row in enumerate(features):
+			outcome = self.predict(row)
+			diff = self.learning_rate * (labels[idx] - outcome)
+
+			self.weights = [w + diff * x for x, w in zip([self.bias] + row, self.weights)]
+			print(self.weights)
+
+	def predict(self, feature):
+		inp = sum([x * w for x, w in zip([self.bias] + feature, self.weights)])
+		
+		if inp > self.threshold:
+			return 1
+		else:
+			return 0
+
+	# test_set is a list of lists with labels and features [[label, feature1, ...], ...]
+	def test(self, test_set):
+		labels = self.labels_to_numbers([row[0] for row in test_set])
+		features = self.features_to_numbers([row[1:] for row in test_set])
+
+		num_cases = len(test_set)
+		num_correct = sum([1 for idx, row in enumerate(features) if labels[idx] == self.predict(row)])
+
+		return num_correct / num_cases
+
 def main():
 	trainset_name, testset_name = get_args()
 
@@ -57,7 +106,10 @@ def main():
 		_, num_test_examples, _, _, _ = load_data(scanner)
 		test_set = load_example(scanner, num_test_examples)
 
-	print(train_set)
-	print(test_set)
+	perceptron = Perceptron(num_features, feature_names, feature_values, label_values, learning_rate=1)
+
+	perceptron.train(train_set)
+
+	print(perceptron.test(test_set))
 
 main()
